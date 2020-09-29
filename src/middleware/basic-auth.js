df92f7e16682cd2770fcec5fa6dcc37d8fa98794
Bryson Creighton <brysoncreighton@gmail.com>
2020-09-29 15:41:30 -0600

    Refactor protected endpoints
    Clean up code to place authentication in a service object and refactor the remaining assets.

--- src/middleware/basic-auth.js
+++ src/middleware/basic-auth.js
@@ -1,3 +1,5 @@
+const AuthService = require('../auth/auth-service')
+
 const requireAuth = (req, res, next) => {
   console.log('requireAuth')
   console.log(req.get('Authorization'))
@@ -14,10 +16,7 @@ const requireAuth = (req, res, next) => {
     basicToken = authToken.slice('basic '.length, authToken.length)
   }
 
-  const [tokenUserName, tokenPassword] = Buffer
-    .from(basicToken, 'base64')
-    .toString()
-    .split(':')
+  const [tokenUserName, tokenPassword] = AuthService.parseBasicToken(basicToken)
 
   if (!tokenUserName || !tokenPassword) {
     return res.status(401).json({
@@ -25,15 +24,17 @@ const requireAuth = (req, res, next) => {
     })
   }
 
-  req.app.get('db')('blogful_users')
-    .where({ user_name: tokenUserName })
-    .first()
+  AuthService.getUserWithuserName(
+    req.app.get('db'),
+    tokenUserName
+  )
     .then(user => {
       if (!user || user.password !== tokenPassword) {
         return res.status(401).json({
           error: 'Unauthorized request'
         })
       }
+      req.user = user
       next()
     })
     .catch(next)
